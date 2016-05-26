5667a20e89b001d901d9fee1a02458a6b3da45f0
skylink94 <henry_parra1994@hotmail.com>
2016-05-26 10:04:22 -0500

    ejercicios

---
+++ html/html/codigo.js
@@ -0,0 +1,8 @@
+
+var nombre = prompt('cual es su nombre?');
+
+if(nombre == 'sebastian') {
+	alert('Bienvenido: '+nombre+' el instructor');
+} else {
+	alert('Bienvenido: '+nombre+' el aprendiz');
+}
